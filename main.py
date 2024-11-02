150e50f1ae772709711176413253754660af290e
Shahbaz Ali <161756431+Pakistan971@users.noreply.github.com>
2024-11-02 09:36:09 +0500

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI(
     description = "Predicting Water Potability"
 )
 
-with open(r"C:\Users\Shahbaz\Desktop\MLops\Video 2 Project\model.pkl","rb") as f:
+with open(r"model.pkl","rb") as f:
     model = pickle.load(f)
 
 @app.get("/")
